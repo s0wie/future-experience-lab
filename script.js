354701a5bddf8b35e840999f37fd83073b4d6f79
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-03-01 13:59:46 +0100

    Om oss-section

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ menuIcon.addEventListener("click", () => {
   menuCross.classList.toggle("hidden");
   menuBurger.classList.toggle("hidden");
 });
+
+// const firstLab = document.querySelector(".first-lab");
+// const secondLab = document.querySelector(".second-lab");
+// const thirdLab = document.querySelector(".third-lab");
+
+// window.addEventListener("scroll", function (event) {
+//   if (
+//     isInViewPort(firstLab) ||
+//     isInViewPort(secondLab) ||
+//     isInViewPort(thirdLab)
+//   ) {
+//     firstLab.classList.remove("opacity");
+//     secondLab.classList.remove("opacity");
+//     thirdLab.classList.remove("opacity");
+//   }
+// });
